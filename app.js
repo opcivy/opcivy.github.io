0bb3b26848d6cb02d7441a99eb2ba477f742b4be
opcivy <kitsunora0906@gmail.com>
2022-11-06 23:53:01 -0700

    work for 11/6

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ $(".link").hover(
         $(this).removeClass('yel').addClass('whi');
     }
 )
+
+let quotes = ["not an anti or a pro-shipper but a third secret thing (adult with job)", "alien, yes i'm an alien!", "\"you should know better\" i dont know ANYTHING!"];
+let select = quotes[Math.floor(Math.random() * quotes.length)];
+$("#quote").html(select);
+
+function penisMode() {
+    $("#titleText").html("PENIS!!!");
+    $("#titleText").css({
+        "font-size": "26pt",
+        "color": "red"
+    })
+    $("#activationText").css("visibility", "visible");
+}
