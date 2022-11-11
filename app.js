5c4ccf62dc31516686fa278a1eb077f60c8d1c02
opcivy <kitsunora0906@gmail.com>
2022-11-11 16:39:06 -0700

    pretty much finished

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // loading animation
 let buttons = gsap.from('.button', {opacity:0, ease:"power1.out", y:"3vw", duration:2});
+let title = gsap.from('#titleText', {opacity:0, duration: 1, y:"-3vw", ease:"power1.out"});
 
 // hover color classes
 $(".link").hover(
@@ -11,15 +12,12 @@ $(".link").hover(
     }
 )
 
-let quotes = ["not an anti or a pro-shipper but a third secret thing (adult with job)", "alien, yes i'm an alien!", "\"you should know better\" i dont know ANYTHING!"];
+let quotes = ["not an anti or a pro-shipper but a third secret thing (adult with job)",
+ "\"you should know better\" i dont know ANYTHING!",
+ "You've listened to their stories and troubles, but what of your own?"];
 let select = quotes[Math.floor(Math.random() * quotes.length)];
 $("#quote").html(select);
 
-function penisMode() {
-    $("#titleText").html("PENIS!!!");
-    $("#titleText").css({
-        "font-size": "26pt",
-        "color": "red"
-    })
-    $("#activationText").css("visibility", "visible");
-}
+let names = ["azzy", "ivy", "vi"];
+let nameSelect = names[Math.floor(Math.random() * names.length)];
+$("#name").html(nameSelect);
